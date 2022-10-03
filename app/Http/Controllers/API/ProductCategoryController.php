2edb79cd4590197fcd2291b1156cad966f769eec
Kirara02 <fathul.sirnaraja92@gmail.com>
2022-10-03 19:40:19 +0700

    second commit

---
+++ app/Http/Controllers/API/ProductCategoryController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Models\ProductCategory;
+use App\Http\Controllers\Controller;
+use App\Helpers\ResponseFormatter;
+
+class ProductCategoryController extends Controller
+{
+    public function all(Request $request){
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('name');
+        $show_product = $request->input('show_product');
+
+        if($id){
+            $kategory = ProductCategory::with(['products'])->find($id);
+
+            if($kategory){
+                return ResponseFormatter::success(
+                    $kategory,
+                    'Data kategory berhasil diambil'
+                );
+            }
+            else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data kategory tidak ada',
+                    404
+                );
+            }
+
+        }
+        $kategory = ProductCategory::query();
+    
+        if($name){
+            $kategory->where('name', 'like', '%'.$name.'%');
+        }
+
+        if($show_product){
+            $kategory->with('products');
+        }
+        
+        return ResponseFormatter::success(
+            $kategory->paginate($limit),
+            'Data list kategori berhasil diambil'
+        );
+    }
+}
